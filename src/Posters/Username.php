71a3c601ca0a616f25b38b5f15015d775c0c925d
Luis Montealegre <montealegreluis@gmail.com>
2018-07-21 00:46:26 -0500

    test: Describe a poster's username
    - Add PHPUnit configuration

---
+++ src/Posters/Username.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+
+namespace Ads\Posters;
+
+use Webmozart\Assert\Assert;
+
+class Username
+{
+    private const MIN_LENGTH = 5;
+    private const FORMAT     = '/^[a-zA-Z0-9_]+$/';
+
+    /** @var string */
+    private $username;
+
+    public function __construct(string $username)
+    {
+        $this->setUsername($username);
+    }
+
+    public function asText(): string
+    {
+        return $this->username;
+    }
+
+    private function setUsername(string $username): void
+    {
+        Assert::notEmpty($username, 'The username should not be blank.');
+        Assert::minLength($username, self::MIN_LENGTH, 'The username should contains at least %2$s characters.');
+        Assert::regex($username, self::FORMAT, 'The username is invalid.');
+        $this->username = $username;
+    }
+}
